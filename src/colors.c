feaebfc2c183ab712a2dc9a3a20fa725175668cf
Edwin Liang Tang <edwinltang@gmail.com>
2020-10-05 21:28:39 -0600

    Fix color functions

--- src/colors.c
+++ src/colors.c
@@ -3,22 +3,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+static gdouble wrap_hue(gdouble h) {
+    gdouble result = fmod(h, 360);
+    if (h < 0.0) h += 360.0;
+    return result;
+}
+
 GdkRGBA hsv_to_rgb(gdouble h, gdouble s, gdouble v) {
-    h = fmod(h, 360);
+    h = wrap_hue(h);
+    s = CLAMP(s, 0.0, 1.0);
+    v = CLAMP(v, 0.0, 1.0);
+
     gdouble c = v * s;
     gdouble x = c * (1 - fabs(fmod(h / 60, 2) - 1));
     gdouble m = v - c;
 
-    int region = (int) floor(h / 60.0);
-    switch (region) {
-        case 0: return (GdkRGBA) {red: c+m, green: x+m, blue: m};
-        case 1: return (GdkRGBA) {red: x+m, green: c+m, blue: m};
-        case 2: return (GdkRGBA) {red: m,   green: c+m, blue: x+m};
-        case 3: return (GdkRGBA) {red: m,   green: x+m, blue: c+m};
-        case 4: return (GdkRGBA) {red: x+m, green: m,   blue: c+m};
-        case 5: return (GdkRGBA) {red: c+m, green: m,   blue: x+m};
+    switch ((int) floor(h / 60.0)) {
+        case 0: return (GdkRGBA) {red: c+m, green: x+m, blue: m,   alpha: 1.0};
+        case 1: return (GdkRGBA) {red: x+m, green: c+m, blue: m,   alpha: 1.0};
+        case 2: return (GdkRGBA) {red: m,   green: c+m, blue: x+m, alpha: 1.0};
+        case 3: return (GdkRGBA) {red: m,   green: x+m, blue: c+m, alpha: 1.0};
+        case 4: return (GdkRGBA) {red: x+m, green: m,   blue: c+m, alpha: 1.0};
+        case 5: return (GdkRGBA) {red: c+m, green: m,   blue: x+m, alpha: 1.0};
         default:
-            return (GdkRGBA) {red: 0, green: 0, blue: 0};
+            return (GdkRGBA) {red: 0, green: 0, blue: 0, alpha: 0};
     }
 }
 
